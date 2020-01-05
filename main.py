61db215593d3b70b7317a97832c7deaa1e7eb6f8
Stefan Gherman <scgheman@gmail.com>
2020-01-05 07:34:00 +0200

    Add styles via CSS.

--- main.py
+++ main.py
@@ -66,5 +66,10 @@ def delete_applicant():
     return redirect(url_for('index'))
 
 
+@app.errorhandler(404)
+def process_404(e):
+    return render_template('404.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
